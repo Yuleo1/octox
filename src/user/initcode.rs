2fa19fac4b8b4eb62914f3aec4fc02a9aa32317f
Hayato Ohhashi <o8@vmm.dev>
2022-11-12 00:40:30 +0900

    src/user/Cargo.toml

---
+++ src/user/initcode.rs
@@ -0,0 +1,8 @@
+#![no_std]
+//#![no_main]
+
+use ulib::usys::{exec, exit};
+
+static init: &str = "init";
+
+pub fn main() {}
--- src/user/lib.rs
+++ src/user/lib.rs
@@ -1,4 +1,11 @@
 #![no_std]
 
+use core::panic;
+
 pub mod stat;
 pub mod usys;
+
+#[panic_handler]
+fn panic(info: &panic::PanicInfo<'_>) -> ! {
+    loop {}
+}
